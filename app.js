3fdceca6ab674bc2b0d94833cd1608011a644c4c
shakil ahmed <shakiltha338@gmail.com>
2024-04-20 08:25:06 +0600

    best seller section for mobile complete

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const menu = document.getElementsByClassName("menu")[0];
 const sidebar = document.getElementsByClassName("sidebar")[0];
 const aside = document.getElementsByClassName("aside")[0];
 
+// if (window.innerWidth > 576 && window.screen.width > 576) {
+//   sidebar.style.display = "none";
+// }
+
 menu.addEventListener("click", (e) => {
   e.preventDefault();
   sidebar.classList.toggle("sidebarExpand");
