328f156cc1e02f5347a93880358565d1e1346633
shakil ahmed <shakiltha338@gmail.com>
2024-04-20 07:02:03 +0600

    completed animated navigation sidebar

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("it is working");
+const menu = document.getElementsByClassName("menu")[0];
+const sidebar = document.getElementsByClassName("sidebar")[0];
+const aside = document.getElementsByClassName("aside")[0];
+
+menu.addEventListener("click", (e) => {
+  e.preventDefault();
+  sidebar.classList.toggle("sidebarExpand");
+  if ([...sidebar.classList].includes("sidebarExpand")) {
+    aside.style.display = "flex";
+  } else {
+    aside.style.display = "none";
+  }
+});
